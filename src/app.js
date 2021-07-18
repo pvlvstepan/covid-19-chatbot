0f1e60348805add1ed2d336eb9f8a332f00a6517
Stepan Pavlov <pvlv.stepan@gmail.com>
2021-07-18 10:34:05 +0800

    Edit start message

--- src/app.js
+++ src/app.js
@@ -4,7 +4,12 @@ const { Telegraf } = require('telegraf');
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.start((ctx) => {
-  ctx.reply("Bot is starting...");
+  const first_name = ctx.message.from.first_name;
+  const last_name = ctx.message.from.last_name;
+
+  const msgFrom = `${first_name}${last_name ? ` ${last_name}` : ``}`;
+
+  ctx.replyWithHTML(`Hi there, <b>${msgFrom}</b>, this is a chatbot that will help you track data and statistics of the latest COVID-19 cases around the globe.\n\n To begin, choose what action you want to do next:\n\n /worldwide - View worldwide cases data\n /country - View specific country cases data\n /location - View cases data in the country of your location (We would require access to your location)\n /help - View list of available commands`);
 });
 
 bot.help((ctx) => {
