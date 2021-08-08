1ab352873beabbb17c76d8cf311cf3b6535ff089
Stepan Pavlov <pvlv.stepan@gmail.com>
2021-08-08 10:56:33 +0800

    Deploy bot

--- src/app.js
+++ src/app.js
@@ -1,13 +1,11 @@
-require('dotenv').config();
+//require('dotenv').config();
+// { Telegraf } = require("telegraf");
+const { Composer } = require('micro-bot');
 
-const { Telegraf } = require("telegraf");
-//const { Composer } = require('micro-bot');
-const bot = new Telegraf(process.env.BOT_TOKEN);
-
+//const bot = new Telegraf(process.env.BOT_TOKEN);
+const bot = new Composer();
 const startMenu = require('./start/index.js');
-
 startMenu(bot);
 
-bot.launch();
-
-//module.exports = bot;
+//bot.launch();
+module.exports = bot;
